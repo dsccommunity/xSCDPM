13b5e28e6b6877bb7aa75024fe5195f6dfe288f5
Indhu Sivaramakrishnan <indhukrishna@live.com>
2016-08-11 21:25:58 -0700

    Added param block to suppress PSSA rule

--- Examples/SCDPM-MultiInstance.ps1
+++ Examples/SCDPM-MultiInstance.ps1
@@ -1,5 +1,9 @@
 #requires -Version 5
 
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingConvertToSecureStringWithPlainText', '')]
+param
+()
+
 Configuration DPM
 {
     Import-DscResource -Module xDismFeature
